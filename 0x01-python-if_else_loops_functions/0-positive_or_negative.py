10e58394d308bfbc1d4d56aced81d5b5bb85d13b
Kholofelo J <kholofelojmocheko@gmail.com>
2022-08-27 14:13:05 +0200

    Task 0, positive or negative and README file

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+	print(number, "is positive")
+	print("")
+	if number == 0:
+		print(number, "is zero")
+		print("")
+else:
+	number < 0
+	print(number, "is negative")
+	print("")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,6 @@
+# 0x01. Python - if/else, loops, functions
+
+
+### 0. Positive anything is better than negative nothing
+
+This program will assign a random signed number to the variable number each time it is executed
