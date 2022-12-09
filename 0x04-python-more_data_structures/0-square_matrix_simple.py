c4d176829faf6a3ae738619d435c707f050bd1e8
Kholofelo Mocheko <kholofelojmocheko@gmail.com>
2022-12-09 16:58:03 +0200

    hot fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    squared =[]
+    squared = []
     for x in matrix:
         matrix_2 = map(lambda i: i**2, x)
         squared.append(list(matrix_2))
     return squared
-    
