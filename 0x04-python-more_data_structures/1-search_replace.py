b979237cc455c82a8999a1704b3f4d72282b6076
Kholofelo Mocheko <kholofelojmocheko@gmail.com>
2022-12-09 22:36:55 +0200

    Hot fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/pythton3
 
 def search_replace(my_list, search, replace):
-    result = [number if number != search else replace for number in my_list]
-    return result
+    my_list = [number if number != search else replace for number in my_list]
+    return my_list
